eab5691d193c0fb9ac66078186755cafd8bb31f8
Abolade Greatness <49532326+Grtnxhor@users.noreply.github.com>
2021-11-15 17:50:56 +0100

    new update

--- staff/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("functions/init.php");
-
-session_unset();
-header("location: ./login");
-
-// redirect("login.php");
-?>
--- staff/test.php
+++
@@ -1,7 +0,0 @@
-<?php 
-include("functions/init.php");
-
-$sql= "SELECT * FROM `result` WHERE `admno` = '$data' AND `term` = '$term' AND `ses` = '$ses'";
- $result_set = query($sql);
- echo row_count($result_set);
- 
