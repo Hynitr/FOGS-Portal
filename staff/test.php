b516f45520fe6988cf7a1e9c90f046662e165238
Abolade Greatness <49532326+Grtnxhor@users.noreply.github.com>
2021-12-21 14:10:12 +0100

    fixed

--- staff/test.php
+++ staff/test.php
@@ -1,7 +1,4 @@
 <?php 
-include("functions/init.php");
+$a = md5('temi@2006.');
 
-$sql= "SELECT * FROM `result` WHERE `admno` = '$data' AND `term` = '$term' AND `ses` = '$ses'";
- $result_set = query($sql);
- echo row_count($result_set);
- 
+echo $a;
